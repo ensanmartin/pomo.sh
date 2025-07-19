10be2d27a4bf6ea7346fe59b9e17551adf152c29
ensanmartin <100975653+ensanmartin@users.noreply.github.com>
2025-07-19 05:02:19 -0300

    Initial commit

---
+++ pomo.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+to_seconds() {
+	local INPUT_MINUTES=$1
+	local INPUT_SECONDS=$((INPUT_MINUTES*60))
+	
+	echo $INPUT_SECONDS
+}
+
+count() {
+	local MIN=$1
+	local SECONDS=$(to_seconds MIN)
+
+	#echo $MIN
+	#echo $SECONDS
+
+	ITERATOR=0
+	CURR_MIN=0
+	CURR_SEC=0
+
+	while [ $ITERATOR -le $SECONDS ]; do
+		if [ $CURR_SEC -ge 60 ]; then
+			((CURR_MIN++))
+			CURR_SEC=0
+		fi
+
+		if [ $CURR_SEC -lt 10 ]; then
+			printf "\r$CURR_MIN:0$CURR_SEC"
+		else
+			printf "\r$CURR_MIN:$CURR_SEC"
+		fi
+		
+		((ITERATOR++))
+		((CURR_SEC++))
+		#sleep 0.1
+		sleep 1
+	done
+
+	printf "\n"
+}
+
+pomodoro() {
+	local POMO=$1
+	local REST=$2
+	#local LONG_REST=$3
+	#local INTERVAL=$4
+
+	echo "Welcome to a $POMO/$REST pomodoro."
+	
+	echo "FOCUS!"
+	count $POMO
+
+	echo "REST!"
+	count $REST
+}
+
+POMO=$1
+REST=$2
+#LONG_REST=$3
+#INTERVAL=$4
+
+pomodoro $POMO $REST $INTERVAL
+#count $POMO
